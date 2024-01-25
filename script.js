bc8cb2c9209b6fda97f0878deb3c023f13aaa47c
SenaNilo <98361804+SenaNilo@users.noreply.github.com>
2024-01-25 19:35:41 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+
+    localStorage.setItem("key", "value");
+    
+}
